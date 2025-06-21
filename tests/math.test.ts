b81d6349c085210e60564a67802e45d683509e4d
Facu Lopez <facundolopez026@gmail.com>
2025-06-21 16:17:25 -0300

    change-test

--- tests/math.test.ts
+++ tests/math.test.ts
@@ -6,7 +6,7 @@ test('add 2 + 3 = 5', () => {
 });
 
 test('substract 5 - 3 = 2', () => {
-    expect(subtract(5, 3)).toBe(2);
+    expect(subtract(5, 3)).toBe(80);
 });
 
 test('multiply 4 * 2 = 8', () => {
