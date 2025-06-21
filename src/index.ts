7dc06b3f193a46a4281cdc06f3ee4fabd83e1f8e
Facu Lopez <facundolopez026@gmail.com>
2025-06-21 16:11:59 -0300

    fix-jest

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 import express from 'express';
+import { add, multiply, subtract } from './math';
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+
+app.get('/add-function', (req, res) => {
+    res.send(add(2, 3).toString());
+});
+
+app.get('/subtract-function', (req, res) => {
+    res.send(subtract(5, 3).toString());
+});
+
+app.get('/multiply-function', (req, res) => {
+    res.send(multiply(4, 2).toString());
 });
 
 app.listen(3000, () => {
--- tests/math.test.ts
+++ tests/math.test.ts
@@ -1,14 +1,14 @@
 import { add, subtract, multiply } from '../src/math';
 import { test, expect } from '@jest/globals';
 
-test('sumar 2 + 3 = 5', () => {
+test('add 2 + 3 = 5', () => {
     expect(add(2, 3)).toBe(5);
 });
 
-test('restar 5 - 3 = 2', () => {
+test('substract 5 - 3 = 2', () => {
     expect(subtract(5, 3)).toBe(2);
 });
 
-test('multiplicar 4 * 2 = 8', () => {
+test('multiply 4 * 2 = 8', () => {
     expect(multiply(4, 2)).toBe(8);
 });
